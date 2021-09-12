d5fb6a90239c3fce5559d2a622598c9b5550e784
Erick henrique <erick.h.a@hotmail.com>
2021-09-12 20:45:32 -0300

    Pagination

--- FrontEnd/src/types/sale.ts
+++ FrontEnd/src/types/sale.ts
@@ -1,3 +1,28 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+            id: number,
+            visited: number,
+            deals: number,
+            amount: number,
+            date: string,
+            seller: Seller
+}
+
+export type SalePage = {
+    content?: Sale[];
+
+    last: boolean;
+    totalPages: number
+    totalElements: number
+    size?: number
+    number: number
+    first: true
+    numberOfElements?: number
+    empty?: boolean;
+}
+
+
 export type SaleSum = {
     sum: number,
     sellerName: string
